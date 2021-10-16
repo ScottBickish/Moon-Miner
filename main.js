f5329a9b0d8a435a4229b1555a03b0a2c4cdc40b
Scott Bickish <sbickish3@gmail.com>
2021-10-15 21:03:02 -0600

    progress

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let currentClickCount = 500
+let currentClickCount = 40000
 let totalmultiplier = 1
+
 let upgrade1 = {
     price: 50,
     multiplier: 1,
@@ -10,33 +11,38 @@ let upgrade2 = {
     multiplier: 50,
     qty: 0,
 }
-
-
+let upgrade3 = {
+    price: 2500,
+    multiplier: 100,
+    qty: 0,
+}
+let upgrade4 = {
+    price: 10000,
+    multiplier: 250,
+    qty: 0,
+}
 
 function mine(){
     currentClickCount += totalmultiplier
-    document.getElementById('clicks').innerText = `current clicks are ${currentClickCount}`
+    document.getElementById('clicks').innerText = `Current clicks are ${currentClickCount}`
 
-    
-}
+    }
+
 function click1(){
     if(currentClickCount >= upgrade1.price){
        totalmultiplier += upgrade1.multiplier
         currentClickCount -= upgrade1.price
         upgrade1.qty++
         upgrade1.price += upgrade1.price
-        
-    }
-    
+        }
     update()
-    
-}
+    }
 
 function update(){
-    document.getElementById('upgrade1').innerText = `click +1 is ${upgrade1.qty}`
-    document.getElementById('c1-price').innerText = `cost: ${upgrade1.price}`
-    document.getElementById('click-tally').innerText = `total click power is ${totalmultiplier}`
-    document.getElementById('clicks').innerText = `current clicks are ${currentClickCount}`
+    document.getElementById('upgrade1').innerText = `Cheese knives purchased ${upgrade1.qty}`
+    document.getElementById('c1-price').innerText = `Cost: ${upgrade1.price}`
+    document.getElementById('click-tally').innerText = `Total click power is ${totalmultiplier}`
+    document.getElementById('clicks').innerText = `Current clicks are ${currentClickCount}`
 }
 
 function click2(){
@@ -45,14 +51,52 @@ function click2(){
          currentClickCount -= upgrade2.price
          upgrade2.qty++
          upgrade2.price += upgrade2.price
+        }
+        update2()
          
-     }
-     update2()
 }
 function update2(){
-    document.getElementById('upgrade2').innerText = `click +50 is ${upgrade2.qty}`
-    document.getElementById('c2-price').innerText = `cost: ${upgrade2.price}`
-    document.getElementById('click-tally').innerText = `total click power is ${totalmultiplier}`
-    document.getElementById('clicks').innerText = `current clicks are ${currentClickCount}`
+    document.getElementById('upgrade2').innerText = `Cheese wheels purchased ${upgrade2.qty}`
+    document.getElementById('c2-price').innerText = `Cost: ${upgrade2.price}`
+    document.getElementById('click-tally').innerText = `Total click power is ${totalmultiplier}`
+    document.getElementById('clicks').innerText = `Current clicks are ${currentClickCount}`
+}
+
+function click3(){
+    if(currentClickCount >= upgrade3.price){
+        currentClickCount -= upgrade3.price
+         upgrade3.qty++
+         upgrade3.price += upgrade3.price
+         }
+    setInterval(() => {
+         currentClickCount += upgrade3.multiplier;
+          update3()
+    }, 3000);
+    
+}
+function update3(){
+    document.getElementById('upgrade3').innerText = `Idle rookie is ${upgrade3.qty}`
+    document.getElementById('c3-price').innerText = `Cost: ${upgrade3.price}`
+    document.getElementById('click-tally').innerText = `Total click power is ${totalmultiplier}`
+    document.getElementById('clicks').innerText = `Current clicks are ${currentClickCount}`
+}
+
+function click4(){
+    if(currentClickCount >= upgrade4.price){
+        currentClickCount -= upgrade4.price
+         upgrade4.qty++
+         upgrade4.price += upgrade4.price
+         }
+    setInterval(() => {
+         currentClickCount += upgrade4.multiplier;
+          update4()
+    }, 3000);
+    
+}
+function update4(){
+    document.getElementById('upgrade4').innerText = `Idle boss is ${upgrade4.qty}`
+    document.getElementById('c4-price').innerText = `Cost: ${upgrade4.price}`
+    document.getElementById('click-tally').innerText = `Total click power is ${totalmultiplier}`
+    document.getElementById('clicks').innerText = `Current clicks are ${currentClickCount}`
 }
 
